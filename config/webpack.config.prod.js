b88ec62ee7d139e866f5784227348f14f2db3498
김민섭 <seidin1@naver.com>
2021-03-26 08:31:21 +0900

    devDependencies->dependencies 변경 js파일 ->jsx 변경

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.jsx",
   output: {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "../build")
